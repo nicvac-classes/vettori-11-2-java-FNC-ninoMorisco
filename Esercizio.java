21a84747c2e2fe14f0e9175c4c8e032332d7fb76
FNC-ninoMorisco <giov.morisco@gmail.com>
2025-01-09 18:01:05 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -8,18 +8,36 @@ class Esercizio {
     // Il programma parte con una chiamata a main().
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
+       Scanner in = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+       int n;
+       int duplicati;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+       System.out.print("Inserire la granzedda del vettore: ");
+       n = in.nextInt();
+       int v[] = new int[n];
+       for(int i = 0; i < n; i++){
+        System.out.print("Inserire il " + (i+1) + " numero: ");
+        v[i] = in.nextInt();
+       }
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+       boolean visitato[] = new boolean[n];
+
+       for(int i = 0; i < n; i++){
+            if(!visitato[i]){
+                duplicati = 1;
+                for(int j = i+1; j < n; j++){
+                    if(v[i] == v[j]){
+                        duplicati++;
+                        visitato[j] = true;
+                        System.out.println("Il valore " + v[i] + " occorre nelle posizioni " + i + "," + j );
+                    }
+                }
+                if(duplicati > 1){
+                    System.out.println("E' stato inserito " + duplicati + " volte");
+                }
+            }
+        }
     }
 }
 
